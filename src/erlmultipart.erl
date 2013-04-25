d4bfef5fa8fa2ee64ab2baa04aeb7c9387491a4c
Markus Ecker <markus.ecker@gmail.com>
2013-04-25 23:54:27 +0200

    cleanup

--- src/erlmultipart.erl
+++ src/erlmultipart.erl
@@ -10,77 +10,90 @@ new(Boundary, FileHandler, MaxSize) when is_binary(Boundary) ->
     FirstBoundary = <<"--", Boundary/binary, "\r\n">>,
     Parser = 
     fun 
+        % autogrow when the buffer is too small
+        (P, S, Buffer, Result) when S /= next, byte_size(Buffer) < byte_size(Boundary) + 6 -> 
+            grow(P,S,Buffer,Result, MaxSize);
+            
+        % need 4 bytes to figure out the end
         (P, next, Buffer, Result) when byte_size(Buffer) < 4 ->
             grow(P,next,Buffer,Result, MaxSize);
-            
+        
+        % just matched a boundary, got end character sequence - return result
         (P, State = next, Buffer = <<"--\r\n">>, Result) ->
             result(P,State,Buffer,Result);
-            
+        
+        % no end sequence, parse next headers
         (P, next, Buffer, Result) ->
             parse(P,headers,Buffer,Result);
-            
-        (P, S, Buffer, Result) when byte_size(Buffer) < byte_size(Boundary) + 6 -> 
-            % autogrow when Buffer is too small
-            grow(P,S,Buffer,Result, MaxSize);
         
+        % skip until first boundary
         (P, initial, Buffer, Result) ->
-            % skip until first boundary
+            
             case binary:split(Buffer,FirstBoundary) of
                 [_] ->
                     skip(P, initial, Buffer, Result, FirstBoundary);
                 [_,Rest] ->
                     parse(P, headers, Rest, Result)
             end;
-            
+        
+        % look for the end of the header block, if not found grow the header buffer
         (P, headers, Buffer, Result) ->
-            % look for \r\n\r\n - if not found request more to parse the whole header
-
+            
             case binary:split(Buffer,<<"\r\n\r\n">>) of
                 [_] ->
                     grow(P,headers,Buffer,Result, MaxSize);
+                    
                 [HeaderBuffer,Rest] ->
                     case parse_headers(HeaderBuffer) of
+                        
                         {ok, Headers} ->
+                           
                             % parse Content Disposition Header
                             CD = proplists:get_value(<<"Content-Disposition">>, Headers, <<"undefined">>),
-                            CDParsed = binary:split(CD, <<";">>,[trim, global]),
-                            case CDParsed of        
+                            case binary:split(CD, <<";">>,[trim, global]) of        
+                                
                                 [<<"form-data">>| KVStrings] ->
+                                    
                                     KVs = parse_kv(KVStrings),
                                     NameVal = proplists:get_value(<<"name">>, KVs),
                                     FilenameVal = proplists:get_value(<<"filename">>, KVs),
+                                    
                                     case {NameVal, FilenameVal} of
                                         {undefined, _} -> 
-                                            {error, <<"Invalid content disposition: ", CD/binary>>};
+                                            perror(<<"Invalid content disposition:">>, CD);
+                                            
                                         {Name, undefined} ->
                                             Result1 = {field, Name, undefined},
                                             parse(P, field, Rest, [Result1|Result]);
+                                            
                                         {Name, _Filename} ->
                                             ContentType = proplists:get_value(<<"Content-Type">>, Headers),
                                             State = FileHandler(start, <<>>, {Name, ContentType}, undefined),
                                             Result1 = {file, Name, ContentType, State},
                                             parse(P, file, Rest, [Result1|Result])
                                     end;
+                                    
                                 _ -> 
-                                    {error, <<"Invalid content disposition: ", CD/binary>>}
+                                    perror(<<"Invalid content disposition:">>, CD)
                             end;
                         Error ->
                             Error
                     end
             end;
         
+        % parse a field - look for the next boundary, otherwise grow
         (P, field, Buffer, Result=[{field,Name,_}|Results]) ->
-            % look for the next boundary, otherwise grow
             case binary:split(Buffer,NextBoundary) of
                 [_] ->
                     grow(P,field,Buffer,Result,MaxSize);
                 [Value,Rest] ->
-                    %% form data done - either complete or next section
+                    %% form field complete
                     parse(P,next,Rest,[{field,Name,Value}|Results])
             end;
         
+        % parse a file
         (P, file, Buffer, [{file,Filename,ContentType,State1}|Results]) ->
-            % look for the next boundary, otherwise grow
+            
             case binary:split(Buffer,NextBoundary) of
                 [_] ->
                     {Data, Rest} = consume(Buffer, NextBoundary),
@@ -98,6 +111,8 @@ new(Boundary, FileHandler, MaxSize) when is_binary(Boundary) ->
     fun (Buffer) ->
         Parser(Parser, initial, Buffer, [])
     end.
+
+% Parser functions
 
 % consume the buffer looking for token
 consume(Buffer, Token) ->
@@ -126,24 +141,32 @@ more(P, S, Buffer1, Result, MaxSize) when MaxSize > 0, byte_size(Buffer1) =< Max
     {more, fun (Buffer2) -> P(P, S, <<Buffer1/binary, Buffer2/binary>>, Result) end };
 
 more(_P, _S, _Buffer1, _Result, _MaxSize) ->
-    {error, <<"Maximum buffer size exceeded">> }.
+    perror(<<"Maximum buffer size exceeded">>).
 
-% ltrim
-trim(<<>>) -> <<>>;
-trim(<<" ",Rest/binary>>) -> trim(Rest);
-trim(<<"\t",Rest/binary>>) -> trim(Rest);
-trim(Other) when is_binary(Other) -> Other;
-trim(L) when is_list(L) -> lists:map(fun(B) -> trim(B) end, L).
+% parser error
+perror(Reason) ->
+    {error, Reason}.
+
+perror(Reason, S) ->
+    {error, <<Reason/binary, " ", S/binary>>}.
+
+% Utilities
+
+ltrim(<<>>) -> <<>>;
+ltrim(<<" ",Rest/binary>>) -> ltrim(Rest);
+ltrim(<<"\t",Rest/binary>>) -> ltrim(Rest);
+ltrim(Other) when is_binary(Other) -> Other;
+ltrim(L) when is_list(L) -> lists:map(fun(B) -> ltrim(B) end, L).
 
 parse_headers(Buffer) ->
     HeadersBuffer = binary:split(Buffer, <<"\r\n">>, [global]),
-    HeadersList = lists:map(fun(H) -> trim(binary:split(H, <<":">>, [trim])) end, HeadersBuffer),
+    HeadersList = lists:map(fun(H) -> ltrim(binary:split(H, <<":">>, [trim])) end, HeadersBuffer),
     Result = lists:mapfoldl(
         fun
             ([K,V], ok) -> 
                 {{K, V}, ok};
             ([H], ok) -> 
-                {{error}, {error, <<"Invalid Header: ", H/binary>>}} ;
+                {{error}, perror(<<"Invalid Header:">>, H)} ;
             (_, Error) -> 
                 {{error}, Error}
         end, ok, HeadersList),
@@ -153,19 +176,21 @@ parse_headers(Buffer) ->
     end.
 
 parse_kv(KV) when is_binary(KV) ->
-    case trim(binary:split(KV, <<"=">>)) of
+    case ltrim(binary:split(KV, <<"=">>)) of
         [K,QV] when byte_size(K) >= 1, byte_size(QV) >= 3 ->
             Sz = byte_size(QV),
             case {binary:at(QV,0), binary:part(QV,{1,Sz-2}), binary:at(QV,Sz-1)} of
                 {$", V, $"} -> {K,V};
-                _ -> {error, <<"Invalid key value pair: ", KV>>}
+                _ -> perror(<<"Invalid key value pair:">>, KV)
             end;
         _ ->
-            {error, <<"Invalid key value pair: ", KV>>}
+            perror(<<"Invalid key value pair:">>, KV)
     end;
 
 parse_kv(L) when is_list(L) ->
     lists:map(fun(KV) -> parse_kv(KV) end, L).
+
+% file handling
 
 tmp_filename() ->
     {A,B,C} = now(),
